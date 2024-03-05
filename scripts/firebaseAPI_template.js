4917e35d1a530400f5f1211dec1f4ab78e50883f
anna-dieu <adao10@my.bcit.ca>
2024-03-05 10:09:54 -0800

    added cards

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDn8FivQDs44tUPLH2YSlk3Fy1lkaKtzHo",
+    authDomain: "myhike-b7cb6.firebaseapp.com",
+    projectId: "myhike-b7cb6",
+    storageBucket: "myhike-b7cb6.appspot.com",
+    messagingSenderId: "398306346467",
+    appId: "1:398306346467:web:63fdae9f39e1f571948cd6",
+    measurementId: "G-E7306KNFMF"
 };
 
 //--------------------------------------------
