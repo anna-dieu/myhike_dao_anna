a92f7154246928cd543289d62f422d2ccb67fd9a
anna-dieu <adao10@my.bcit.ca>
2024-03-05 09:57:34 -0800

    added files

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyDn8FivQDs44tUPLH2YSlk3Fy1lkaKtzHo",
+    authDomain: "myhike-b7cb6.firebaseapp.com",
+    projectId: "myhike-b7cb6",
+    storageBucket: "myhike-b7cb6.appspot.com",
+    messagingSenderId: "398306346467",
+    appId: "1:398306346467:web:63fdae9f39e1f571948cd6",
+    measurementId: "G-E7306KNFMF"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
